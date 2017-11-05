1b91f76434e055f3caea7c6a14a465dc026a7bf8
codeavenger1 <league@wts10.attlocal.net>
2017-11-05 15:53:05 -0800

    birthday/cat

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -11,15 +11,35 @@ public class BirthdayReminder {
 	public static void main(String[] args) {
 	
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String momsBirthday = "june 4th";
+		String dadsBirthday = "september 4";
+		String myBirthday = "August 26th";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+	String a = JOptionPane.showInputDialog("who");
 		// 3. Print out what the user typed
-		
+
 		// 4. if user asked for "mom"
+		if (a.equals("mom")) {
+			System.out.println(momsBirthday);
+		}
+		else	if (a.equals("dad")) {
+			System.out.println(dadsBirthday);
+		}
+		else	if (a.equals("me")) {
+			System.out.println(myBirthday);
+			
+		}else{
+			System.out.println("not found in database");
+		}
+		}
+		
+		
+		
+		
+		
+		}
+	
 			//print mom's birthday
 		// 5. if user asked for "dad"
 			// print dad's birthday
@@ -27,5 +47,5 @@ public class BirthdayReminder {
 			// print myBirthday
 		//7. otherwise print "Sorry, i don't remember that person's birthday!"
 
-	} 
-}
+	 
+
