b758762107f61acc617f569ad6908bffc8c77831
zahmed2003 <league@WTS10.attlocal.net>
2017-12-10 14:49:03 -0800

    Shiny objects 12/10/17

--- src/loops/ShinyObjects.java
+++ src/loops/ShinyObjects.java
@@ -5,30 +5,33 @@ package loops;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-
+import javax.swing.JOptionPane;
 
 public class ShinyObjects {
 	public static void main(String[] args) {
 
 		// 2. Ask the user how many shiny objects they want
-
+		String input = JOptionPane.showInputDialog("how many shiny objects");
+		int num = Integer.parseInt(input);
 		// 3. Play the sound that many times
+		for (int i = 0; i < num; i++) {
 
+			playMisterZee();
+		}
 		// 1. Call the method below
 
 	}
 
 	public static void playMisterZee() {
-    	try {
- 
-   		 Clip clip = AudioSystem.getClip();
-   		 clip.open(AudioSystem.getAudioInputStream(ShinyObjects.class.getResource("shiny-objects.wav")));
-   		 clip.start();
-   		 Thread.sleep(3600);
-    	} catch (Exception ex) {
-        	ex.printStackTrace();
-    	}
-	}
+		try {
 
+			Clip clip = AudioSystem.getClip();
+			clip.open(AudioSystem.getAudioInputStream(ShinyObjects.class.getResource("shiny-objects.wav")));
+			clip.start();
+			Thread.sleep(3600);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 
 }
